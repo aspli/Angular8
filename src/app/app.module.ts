f66f64fec2d454da9f3d1021c0448b6a07bbc767
aspli <alexandre.xoquito@gmail.com>
2022-03-24 17:18:20 -0300

    Trabalhando com Rotas, usando as rotas padrões e criando novas rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,34 @@ import { CourseListComponent } from './course/course-list.component';
 import { StarComponent } from './star/star.component';
 import { FormsModule } from '@angular/forms';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav.bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './error404/error-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component
 
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo:'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
