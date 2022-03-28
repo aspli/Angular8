ebec713d747f2b2205b810103420ed13f4d4df08
aspli <alexandre.xoquito@gmail.com>
2022-03-28 16:54:21 -0300

    Finalizado o Curso de Angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { Error404Component } from './error404/error-404.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CourseModule } from './course/course-module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Error404Component 
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -21,9 +18,6 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo:'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
   ],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { Error404Component } from "./component/error404/error-404.component";
 import { NavBarComponent } from "./component/nav-bar/nav.bar.component";
 
 @NgModule({
     declarations: [
-        NavBarComponent
+        NavBarComponent,
+        Error404Component
     ],
     imports: [
-        RouterModule
+        RouterModule.forChild([
+            {
+                path: '', redirectTo:'courses', pathMatch: 'full'
+            },
+            {
+                path: '**', component: Error404Component
+            },
+            
+        ])
     ],
     exports: [
         NavBarComponent
