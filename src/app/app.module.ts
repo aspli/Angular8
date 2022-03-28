6071e2a08241888c2bbb61997f5825aac1e71585
aspli <alexandre.xoquito@gmail.com>
2022-03-28 16:36:41 -0300

    Trabalhando com a pasta Core geralmente para regra de negocio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav.bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from './error404/error-404.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CourseModule } from './course/course-module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component 
 
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    CourseModule,    
+    CourseModule,  
+    CoreModule,  
     RouterModule.forRoot([
       {
         path: '', redirectTo:'courses', pathMatch: 'full'
